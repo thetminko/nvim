bb6109895eab285adbe75132ac4476896e108462
Thet Min Ko <dev.minko@gmail.com>
2023-12-27 09:04:58 +0800

    Copilot non strict ssl

--- custom/configs/copilot.lua
+++ custom/configs/copilot.lua
@@ -1,5 +1,8 @@
 local copilot = require("copilot")
 
+vim.g.copilot_assume_mapped = true
+vim.g.copilot_proxy_strict_ssl = false
+
 copilot.setup({
   suggestion = {
     enabled = true,
--- custom/init.lua
+++ custom/init.lua
@@ -1,2 +1,1 @@
 vim.wo.relativenumber = true
-vim.g.copilot_assume_mapped = true
