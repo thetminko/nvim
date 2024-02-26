87a2f790f5ea017fe123bcdef1b861cc1a701136
Thet Min Ko <min_ko_thet_from.ufinity@tech.gov.sg>
2024-02-26 10:09:24 +0800

    comment out deprecated

--- custom/configs/null-ls.lua
+++ custom/configs/null-ls.lua
@@ -6,8 +6,8 @@ local opts = {
  --   null_ls.builtins.formatting.prettierd,
     -- null_ls.builtins.formatting.deno_fmt,
 
-    null_ls.builtins.diagnostics.eslint_d,
-    null_ls.builtins.code_actions.eslint_d,
+    -- null_ls.builtins.diagnostics.eslint_d,
+    -- null_ls.builtins.code_actions.eslint_d,
 
     null_ls.builtins.code_actions.refactoring,
 
