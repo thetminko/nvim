5cc50abe52e7bdce9adf1b0d5ed424ef031df855
Thet Min Ko <min_ko_thet_from.ufinity@tech.gov.sg>
2024-02-26 23:22:33 +0800

    Setting up copilot and typescript

---
+++ lua/plugins/copilot.lua
@@ -0,0 +1,35 @@
+return {
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    build = ":Copilot auth",
+    event = "InsertEnter",
+    opts = {
+      suggestion = {
+        enabled = true,
+        auto_trigger = true,
+        keymap = {
+          accept = "<Tab><Tab>",
+          next = "<Tab>1",
+          prev = "<Tab>2",
+        },
+      },
+      panel = { enabled = true },
+      filetypes = {
+        javascript = true,
+        typescript = true,
+        typescriptreact = true,
+        javascriptreact = true,
+        lua = true,
+        go = true,
+        rust = true,
+        java = true,
+        sql = true,
+        shell = true,
+        vim = true,
+        yaml = true,
+        dockerfile = true,
+      },
+    },
+  }
+}
