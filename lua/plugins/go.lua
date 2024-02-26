177ae6d7233913981c209e8c380382a8d3e4b4b1
Thet Min Ko <min_ko_thet_from.ufinity@tech.gov.sg>
2024-02-26 23:46:26 +0800

    Merge to go.lua

--- lua/plugins/go.lua
+++ lua/plugins/go.lua
@@ -1,5 +1,16 @@
 return {
   {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, {
+        "go",
+        "gomod",
+        "gowork",
+        "gosum",
+      })
+    end,
+  },
+  {
     "neovim/nvim-lspconfig",
     opts = {
       servers = {
