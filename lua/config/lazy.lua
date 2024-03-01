fe7769423d31202da079cde208a57d7df376c523
Thet Min Ko <min_ko_thet_from.ufinity@tech.gov.sg>
2024-03-01 20:07:55 +0800

    Use default lazyvim extras instead

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -18,6 +18,14 @@ require("lazy").setup({
     { import = "plugins" },
     { import = "lazyvim.plugins.extras.linting.eslint" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
+    { import = "lazyvim.plugins.extras.lang.yaml" },
+    { import = "lazyvim.plugins.extras.lang.tailwind" },
+    { import = "lazyvim.plugins.extras.lang.typescript" },
+    { import = "lazyvim.plugins.extras.lang.markdown" },
+    { import = "lazyvim.plugins.extras.lang.json" },
+    { import = "lazyvim.plugins.extras.lang.docker" },
+    { import = "lazyvim.plugins.extras.lang.terraform" },
+    { import = "lazyvim.plugins.extras.lang.go" },
   },
   defaults = {
     -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
